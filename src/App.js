c38f7bc4dce7587246bad3454ff9fdb2aa6e5eb9
ganeshparuchuri <ganesh3paruchuri@gmail.com>
2023-10-14 11:01:54 +0530

    created-assignment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { BrowserRouter, Route, Switch } from "react-router-dom"
-import Home from "./Components/Home"
-import Form from "./Components/Form"
-import Result from  './Components/Result';
-
+import DashBoardPage from "./Components/DashBoardPage"
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/"  component={Home} />
-        <Route path="/form/:username" component={Form} />
-        <Route path="/result" component={Result} />
-      </Switch>
-    </BrowserRouter>
+    <DashBoardPage/>
   );
 };
 
